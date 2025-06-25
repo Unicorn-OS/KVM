b2a115ccc8705345c26bfe19104e70fee805c420
DarkStar <darkstar@unisocial.net>
2025-06-25 10:14:00 +0800

    try idempotent

--- src/ansible/dev.sh
+++ src/ansible/dev.sh
@@ -4,3 +4,4 @@ echo "*/" > roles/dev/.gitignore
 cd roles/dev
 git clone git@github.com:Unicorn-OS/ansible-role-KVM.git
 git clone git@github.com:Unicorn-OS/ansible-role-Virt-Manager.git
+git clone git@github.com:Unicorn-OS/ansible-role-Libvirt-Network-startOnBoot.git
