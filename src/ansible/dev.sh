ed30689516ebc2c0f7a1cecb63a04e84f085cd0b
MetaBot A.I. <metabot@unisocial.net>
2025-07-22 19:26:29 -0500

    try

--- src/ansible/dev.sh
+++ src/ansible/dev.sh
@@ -2,6 +2,6 @@ mkdir -p roles/dev
 echo "*/" > roles/dev/.gitignore
 
 cd roles/dev
-git clone git@github.com:Unicorn-OS/ansible-role-KVM.git
-git clone git@github.com:Unicorn-OS/ansible-role-Virt-Manager.git
-git clone git@github.com:Unicorn-OS/ansible-role-Libvirt-Network-startOnBoot.git
+git clone https://github.com/Unicorn-OS/ansible-role-KVM.git
+git clone https://github.com/Unicorn-OS/ansible-role-Virt-Manager.git
+git clone https://github.com/Unicorn-OS/ansible-role-Libvirt-Network-startOnBoot.git
